71c8447336829ff16550bf57328d48f59139d500
Yusuf <yusufozmis2@gmail.com>
2026-01-04 16:30:12 +0300

    "feat: define a new ante handler map using collections.Map"

--- x/secondarykeys/keeper/keeper.go
+++ x/secondarykeys/keeper/keeper.go
@@ -1,14 +1,15 @@
 package keeper
 
 import (
+	"context"
+	"example/x/secondarykeys/types"
 	"fmt"
 
 	"cosmossdk.io/collections"
 	"cosmossdk.io/core/address"
 	corestore "cosmossdk.io/core/store"
 	"github.com/cosmos/cosmos-sdk/codec"
-
-	"example/x/secondarykeys/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 type Keeper struct {
@@ -19,8 +20,9 @@ type Keeper struct {
 	// Typically, this should be the x/gov module account.
 	authority []byte
 
-	Schema collections.Schema
-	Params collections.Item[types.Params]
+	Schema         collections.Schema
+	Params         collections.Item[types.Params]
+	AnteHandlerMap collections.Map[sdk.AccAddress, []byte]
 }
 
 func NewKeeper(
@@ -43,6 +45,13 @@ func NewKeeper(
 		authority:    authority,
 
 		Params: collections.NewItem(sb, types.ParamsKey, "params", codec.CollValue[types.Params](cdc)),
+		AnteHandlerMap: collections.NewMap(
+			sb,
+			collections.NewPrefix(0), // or 1, 2, etc if you have multiple maps
+			"ante_handler_map",
+			sdk.AccAddressKey,
+			collections.BytesValue,
+		),
 	}
 
 	schema, err := sb.Build()
@@ -57,4 +66,16 @@ func NewKeeper(
 // GetAuthority returns the module's authority.
 func (k Keeper) GetAuthority() []byte {
 	return k.authority
+}
+
+func (k Keeper) SetSecondaryPubKeyAnteHandler(ctx context.Context, addr sdk.AccAddress, pubKey []byte) error {
+	return k.AnteHandlerMap.Set(ctx, addr, pubKey)
+}
+
+func (k Keeper) GetSecondaryPubKeyAnteHandler(ctx context.Context, addr sdk.AccAddress) ([]byte, error) {
+	bz, err := k.AnteHandlerMap.Get(ctx, addr)
+	if err != nil {
+		return nil, err
+	}
+	return bz, err
 }
